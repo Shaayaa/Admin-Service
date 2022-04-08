48a9a027d582f94e2d5470ff9bdb31b40c2a8e32
Shayaaa <shahaslucifer@gmail.com>
2022-04-08 10:11:15 -0400

    First project setup

---
+++ src/main/java/com/admin/AdminServiceRunner.java
@@ -0,0 +1,16 @@
+package com.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AdminServiceRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(AdminServiceRunner.class, args);
+
+	}
+
+}
